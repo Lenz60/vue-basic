18cb9e16fa4bacb680ea4ed17aa86ba8f8e1254a
Rafly Andrian <raflyandrian@gmail.com>
2024-10-08 22:45:04 +0700

    feat(Account): Lifecycle Hooks
    Lifecycle Hooks examples

--- src/components/Pages/Account.vue
+++ src/components/Pages/Account.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
-    <h1>Account</h1>
+    <input
+      class="input input-bordered input-success w-full max-w-xs"
+      type="text"
+      v-model="account"
+    />
+    <h1>Account: {{ account }}</h1>
   </div>
 </template>
 
 <script>
+import { onMounted, onUpdated, onUnmounted, ref, watch, computed } from "vue";
+
 export default {
   setup() {
-    return {};
+    const preAccountName = ref("John Doe");
+    const account = ref("");
+
+    watch(account, (newValue, oldValue) => {
+      console.log(`Account changed from ${oldValue} to ${newValue}`);
+    });
+
+    const accountName = computed(() => {
+      return preAccountName.value.toUpperCase();
+    });
+
+    onMounted(() => {
+      console.log("Component has been mounted");
+    });
+
+    onUpdated(() => {
+      console.log("Component has been updated");
+    });
+
+    onUnmounted(() => {
+      console.log("Component has been unmounted");
+    });
+
+    return {
+      account,
+      accountName,
+    };
   },
 };
 </script>
